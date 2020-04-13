5e131ddf0774624b90b0435b169722c566709b71
neutron <joshua@green.geek.nz>
2020-04-13 12:19:24 +1200

    light code cleanup

--- robot/src/main.cpp
+++ robot/src/main.cpp
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "sensors/sensors.h"
 
 /**
  * A callback function for LLEMU's center button.
@@ -74,10 +75,6 @@ void autonomous() {}
  * task, not resume it from where it left off.
  */
 void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::Motor left_mtr(1);
-	pros::Motor right_mtr(2);
-
 	while (true) {
 		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
 		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
