2639892437adf2e5c927a4c8a24c901fdd93803f
neutron <joshua@green.geek.nz>
2020-05-03 11:28:28 +1200

    Major Code Refactor + gui app

---
+++ tracking/trackapp.py
@@ -0,0 +1,146 @@
+import kivy 
+import tracking
+import cv2
+import csv
+import numpy as np
+import time 
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+from matplotlib import style
+from kivy.config import Config
+Config.set('graphics', 'resizable', False)
+Config.set('graphics', 'width', '1280')
+Config.set('graphics', 'height', '720')
+Config.set('graphics', 'minimum_width', '1280')
+Config.set('graphics', 'minimum_height', '720')
+
+matplotlib.use('module://kivy.garden.matplotlib.backend_kivy')
+style.use('dark_background')
+kivy.require("1.0.7")
+
+
+
+from kivy.app import App
+from kivy.uix.image import Image
+from kivy.clock import Clock
+from kivy.graphics.texture import Texture
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+
+
+
+class CircularBuffer:
+    def __init__(self, size):
+        self.size = size
+        self.array = []
+    def append(self, value):
+        if not len(self.array) < self.size:
+            del self.array[0]
+        self.array.append(value)
+
+    def get_value(self):
+        total = 0
+        for i in self.array:
+            total += i
+        return total / (len(self.array) or 1)
+
+class KivyTracker(Image):
+    def __init__(self, capture, fps, **kwargs):
+        super(KivyTracker, self).__init__(**kwargs)
+        self.capture = capture
+        self.markerDic = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_7X7_50)
+        self.cameraMatrix = np.loadtxt("calb/cameraMatrix.csv", delimiter=",")
+        self.distCoeffs = np.loadtxt("calb/distCoeffs.csv", delimiter=",")
+        self.app = App.get_running_app()
+
+        self.xa = CircularBuffer(5)
+        self.ya = CircularBuffer(5)
+        self.za = CircularBuffer(5)
+
+        self.pitcha = CircularBuffer(5)
+        self.yawa = CircularBuffer(5)
+        self.rolla = CircularBuffer(5)
+        self.zero = [0, 0, 0, 0, 0, 0]
+        self.file = None
+        self.filewriter= None
+        Clock.schedule_interval(self.update, 1.0 / fps)
+    def togglerecord(self, state):
+        if state == "down" and not self.file:
+            self.file = open("data/{}_data.csv".format(time.time()), "w+", newline='')
+            self.filewriter = csv.writer(self.file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        else:
+            self.filewriter = None
+            self.file.close()
+            self.file = None
+    def update(self, dt):
+        ret, img = self.capture.read()
+        if ret:
+            #
+            img, rvec, tvec = tracking.track_marker(img, self.markerDic, 0, 20, self.cameraMatrix, self.distCoeffs,)
+            #set text icons
+            if rvec and tvec:
+                self.xa.append(tvec[0])
+                self.ya.append(tvec[1])
+                self.za.append(tvec[2])
+
+                self.pitcha.append(rvec[0])
+                self.yawa.append(tvec[2])
+                self.rolla.append(tvec[1])
+                #if csv file open append to csv file
+                if self.filewriter:
+                    self.filewriter.writerow([time.time(), self.xa.get_value() - self.zero[0], self.ya.get_value() - self.zero[1], self.za.get_value() - self.zero[2], self.pitcha.get_value() - self.zero[3], self.yawa.get_value() - self.zero[4], self.rolla.get_value() - self.zero[5]])
+                    self.app.root.graph.datax.append(self.xa.get_value() - self.zero[0])
+
+                #print xyz values na pitch yaw roll
+            self.app.root.ids.vx.text = "X:{:4.1f}".format(self.xa.get_value() - self.zero[0])
+            self.app.root.ids.vy.text = "Y:{:4.1f}".format(self.ya.get_value() - self.zero[1])
+            self.app.root.ids.vz.text = "Z:{:4.1f}".format(self.za.get_value() - self.zero[2])
+
+            self.app.root.ids.ax.text = "Pitch:{:4.1f}".format(self.pitcha.get_value() - self.zero[3])
+            self.app.root.ids.ay.text = "Yaw:{:4.1f}".format(self.yawa.get_value() - self.zero[4])
+            self.app.root.ids.az.text = "Roll:{:4.1f}".format(self.rolla.get_value() - self.zero[5])
+            # convert it to texture
+            buf1 = cv2.flip(img, 0)
+            buf = buf1.tostring()
+            image_texture = Texture.create(
+                size=(img.shape[1], img.shape[0]), colorfmt='bgr')
+            image_texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
+            # display image from the texture
+            self.texture = image_texture
+    def zerovalues(self):
+            self.zero[0] = self.xa.get_value()
+            self.zero[1] = self.ya.get_value()
+            self.zero[2] = self.za.get_value()
+            self.zero[3] = self.pitcha.get_value()
+            self.zero[4] = self.yawa.get_value()
+            self.zero[5] = self.rolla.get_value()
+class RootWidget(Widget):
+    def __init__(self, **kwargs):
+        super(RootWidget, self).__init__(**kwargs)
+        self.capture = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+        self.capture.set(cv2.CAP_PROP_FRAME_WIDTH,1280)
+        self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+        self.tracker = KivyTracker(capture=self.capture, fps=30)
+        self.tracker.size_hint = (1, 0.5)
+        self.ids.v1.add_widget(self.tracker, len(self.ids.v1.children))
+        print(self.ids.v1.children)
+    def on_stop(self):
+        #without this, app will not exit even if the window is closed
+        self.capture.release()
+
+class TrackApp(App):
+    def build(self):
+        widget = RootWidget()
+        return widget
+
+
+Config.set('graphics', 'resizable', False)
+Config.set('graphics', 'width', '1280')
+Config.set('graphics', 'height', '720')
+Config.set('graphics', 'minimum_width', '1280')
+Config.set('graphics', 'minimum_height', '720')
+
+if __name__ == "__main__":
+    TrackApp().run()
--- tracking/tracking.py
+++ tracking/tracking.py
@@ -2,108 +2,70 @@ import cv2
 import csv
 import numpy as np
 import math
-import time 
 
-def zerovalue(zero, value):
-    if zero:
-        for i in range(len(zero)):
-            value[i] -= zero[i]
-    return value
-
-#convert rotationmatrix to eualr angles
-def yawpitchrolldecomposition(Rx):
-    R = Rx[0]
-    sin_x    = math.sqrt(R[2,0] * R[2,0] +  R[2,1] * R[2,1])    
+#soloty to help convert rotation matrix to eualr angle , soultion was found online idk
+def get_eular(Rx):
+    R = Rx
+    sin_x    = math.sqrt(R[2][0] * R[2][0] +  R[2][1] * R[2][1])    
     validity  = sin_x < 1e-6
     if not validity:
-        z1    = math.atan2(R[2,0], R[2,1])     # around z1-axis
-        x      = math.atan2(sin_x,  R[2,2])     # around x-axis
-        z2    = math.atan2(R[0,2], -R[1,2])    # around z2-axis
+        z1    = math.atan2(R[2][0], R[2][1])     # around z1-axis
+        x      = math.atan2(sin_x,  R[2][2])     # around x-axis
+        z2    = math.atan2(R[0][2], -R[1][2])    # around z2-axis
     else: # gimbal lock
         z1    = 0                                         # around z1-axis
-        x      = math.atan2(sin_x,  R[2,2])     # around x-axis
+        x      = math.atan2(sin_x,  R[2][2])     # around x-axis
         z2    = 0                                         # around z2-axis
     return [math.degrees(z1), math.degrees(x), math.degrees(z2)]
 
-#set icon wdiht in cm
-markerWidth = 20
+def detect_marker(img, mId, markersDict, camMatrix, distCoef):
+    #get corners 
+    corners, ids, rejectedimgpoints = cv2.aruco.detectMarkers(img, markersDict, cameraMatrix = camMatrix, distCoeff = distCoef)
+    #if there is a marker of id 0 return that conrer set else return non
+    ids = np.squeeze(np.asarray(ids)).tolist()
+    if ids == list() and mId in ids:
+        return corners[ids.index(mId)]
+    elif ids == 0 and not ids == None:
+        return corners[mId]
+    return np.asarray([])
 
-#inputvideo file
+def get_marker_pose(corners, markerWidth, cameraMatrix, distCoeffs):
+    #estimate the pose of cther single corner 
+    rvecs, tvecs, _objPoints = cv2.aruco.estimatePoseSingleMarkers([corners], markerWidth, cameraMatrix, distCoeffs)
+    #save rogianl values for image procceesing
+    iRvec = rvecs[0]
+    iTvec = tvecs[0]
+    #get the only entry of rvec and tvec and convert to numpy matrix that is reshaped to be 3, 1 
+    rvec = np.matrix(rvecs[0]).reshape((3, 1))
+    tvec = np.matrix(tvecs[0]).reshape((3, 1))
+    
+    #convert from marker rletaive to camer, to camera relative to marker both rvec and tvec
+    #get rogues of rvec
+    R, _ = cv2.Rodrigues(rvec)
+    #transpose r and covert to numpie matrix
+    Rt = np.matrix(R).T
+    #cacluate tvec
+    tvec = np.dot(-Rt, tvec)
+    #convert tvec to array
+    tvec = np.squeeze(np.asarray(tvec)).tolist()
+    #convert rvec to eular angles for ease of use
+    rvec = get_eular(np.asarray(Rt))
+    #return both rvecx and tvec adn the inital rvec and tvec
+    return (rvec, tvec, iRvec, iTvec)
 
-markers = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_7X7_50)
 
-#define camerea
-camera = cv2.VideoCapture(1, cv2.CAP_DSHOW)
-#open first camera
-camera.set(cv2.CAP_PROP_FRAME_WIDTH,1280)
-camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
-#create aruco board
+def track_marker(img, markerDic, markerId, markerWidth, cameraMatrix, distCoeffs):
+    #detect markers
+    outImage = img
+    markerCorner = detect_marker(img, markerId, markerDic, cameraMatrix, distCoeffs)
+    #if markers detected get pose and draw marker items 
+    if markerCorner.any():
+        rvec, tvec, iRvec, iTvec = get_marker_pose(markerCorner, markerWidth, cameraMatrix, distCoeffs)
+        #draw markers and aixis
+        outImage = cv2.aruco.drawDetectedMarkers(outImage, [markerCorner])
+        outImage = cv2.aruco.drawAxis(outImage, cameraMatrix, distCoeffs, iRvec, iTvec, markerWidth/2)
+        #return outimage and rvec and tvec
+        return (outImage, rvec, tvec)
+    #else jsut return outmiane and two nones
+    return (outImage, None, None)
 
-#create video window
-cv2.namedWindow("Video", cv2.WINDOW_NORMAL)
-
-cameraMatrix = np.loadtxt("calb/cameraMatrix.csv", delimiter=",")
-distCoeffs = np.loadtxt("calb/distCoeffs.csv", delimiter=",")
-zerovalues = [0] * 50
-
-with open("data/{}_data.csv".format(time.time()), "w+", newline='') as csvfile:
-    csvfile_writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    while True:
-        #define images
-        image = 0
-        imageCopy = 0
-        ret, image = camera.read()
-        #copy to iamgecopy 
-        imageCopy = image
-        #detectmarkers and outputs imagase
-        #setuop corents
-        corners = []
-        ids = []
-        rejectedimgpoints = []
-
-        #detect markers
-        corners, ids, rejectedimgpoints = cv2.aruco.detectMarkers(
-            image, markers, corners, None, None, None, cameraMatrix, distCoeffs)
-        #chekc if any markers are present
-        if len(corners) > 0:
-            #estimate pose of markers
-            rvecs, tvecs, _objPoints = cv2.aruco.estimatePoseSingleMarkers(corners, markerWidth, cameraMatrix, distCoeffs)
-            #draw detected markes if any present
-            imageCopy = cv2.aruco.drawDetectedMarkers(imageCopy, corners, ids)
-            #iterate over markers (list of rvecs will be the same length as other markers)
-            for i in range(len(rvecs)):
-                rvec = np.matrix(rvecs[i]).reshape((3, 1))
-                tvec = np.matrix(tvecs[i]).reshape((3, 1))
-                R, _ = cv2.Rodrigues(rvec)
-                R = np.matrix(R).T
-                tvec = np.dot(-R, np.matrix(tvec))
-                tvec = np.asarray(tvec)
-                #inverse the perspective
-                #get attute of vectors
-                angle = yawpitchrolldecomposition(cv2.Rodrigues(rvecs[i]))
-                #draw marker
-                #  
-                imageCopy = cv2.aruco.drawAxis(imageCopy, cameraMatrix, distCoeffs, rvecs[i], tvecs[i], markerWidth/2)
-                #print amers postions
-                valuesraw = [angle[0], angle[1], angle[2], tvec[0, 0], tvec[1, 0], tvec[2, 0]]
-                values = zerovalue(zerovalues[ids[i][0]], valuesraw)
-                
-                print("marker:{:04.1f} Rot: {:04.1f}    {:04.1f}   {:04.1f} Trans: {:04.1f}     {:04.1f}    {:04.1f}                        ".format(
-                    ids[i][0], values[0], values[1], values[2], values[3], values[4], values[5], end="\r"))
-                        
-                csvfile_writer.writerow([time.time(), ids[i][0], values[0], values[1], values[2], values[3], values[4], values[5]])
-                key = cv2.waitKey(30)
-                if key == 122:
-                    zerovalues[ids[i][0]] = valuesraw
-            #show on windows
-        cv2.imshow("Video", imageCopy)
-        #relace
-        key = cv2.waitKey(30)
-        if key == 27 :
-            break
-            
-    camera.release()
-    cv2.destroyAllWindows() 
-
-#daft punk intenisfys 
-print("\nend of line")
