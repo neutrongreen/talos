3b9401d45b6403f23a64735f4ab80f11dd0058d6
Neutino <joshua@green.geek.nz>
2020-05-21 09:33:09 +1200

    impliment critic

--- VexAI/model.py
+++ VexAI/model.py
@@ -2,18 +2,41 @@ from tensorflow.keras import Model
 from tensorflow.keras import Model
 from keras.layers import Input, Dense
 from keras.optimizers import Adam
-
-HIDDEN_SIZE = 32
+from ppo import *
+HIDDEN_SIZE = 128
 NUM_LAYERS = 3
+INPUT_SIZE = 81
+OUTPUT_SIZE = 19
+LR = 1e-4  # Lower lr stabilises training greatly
 
-def build_actor():
-    state_input = Input(shape=(27,))
-    advantage = Input(shape=(1,))
-    old_prediction = Input(shape=(4,))
+class Actor():
+    def build_actor(self):
+        state_input = Input(shape=(INPUT_SIZE,))
+        advantage = Input(shape=(1,))
+        old_prediction = Input(shape=(OUTPUT_SIZE,))
 
-    x = Dense(HIDDEN_SIZE, activation='relu')(state_input)
-    for i in range(NUM_LAYERS -1):
-        x = Dense(HIDDEN_SIZE, activation='relu')(x)
-    out_actions = Dense(4, activation='softmax', name='output')(x)
+        x = Dense(HIDDEN_SIZE, activation='relu')(state_input)
+        for i in range(NUM_LAYERS -1):
+            x = Dense(HIDDEN_SIZE, activation='relu')(x)
+        out_actions = Dense(OUTPUT_SIZE, activation='softmax', name='output')(x)
 
-    Model = Model(inputs=[state_input, advantage, old_prediction], outputs=[out_actions])
+        model = Model(inputs=[state_input, advantage, old_prediction], outputs=[out_actions])
+        model.compile(optimizer=Adam(lr=LR),
+            loss=[proximal_policy_optimization_loss(
+                  advantage=advantage,
+                        old_prediction=old_prediction)])
+        model.summary()
+        return model
+
+    def build_critic(self):
+        state_input = Input(shape=(INPUT_SIZE,))
+
+        x = Dense(HIDDEN_SIZE, activation='relu')(state_input)
+        for i in range(NUM_LAYERS -1):
+            x = Dense(HIDDEN_SIZE, activation='relu')(x)
+        out_actions = Dense(1, activation='tanh', name='prediction')(x)
+
+        model = Model(inputs=[state_input], outputs=[out_actions])
+        model.compile(optimizer=Adam(lr=LR), loss='mse')
+        model.summary()
+        return model  
